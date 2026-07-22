b65331f056d30d2b08f854cd48931191617beddf
Juliana-Elisa <juliana_bracero@elcamino.edu>
2026-07-22 00:08:51 -0700

    setting up timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,53 @@
+import {useState, useEffect, useRef} from "react";
 import "./Timer.css";
 
 function Timer() {
+  const WORK_TIME = 25 * 60; // 25 mins in seconds
+
+  const [secondsLeft, setSecondsLeft] = useState (WORK_TIME);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  //countdown
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval (() => {
+        setSecondsLeft ((prev) => {
+          if (prev <= 1) {
+            clearInterval (intervalRef.current);
+            setIsRunning(false);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+
+    // cleanup on pause so intervals don't stack
+    return () => clearInterval(intervalRef.current);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    if (secondsLeft === 0) return; // nothing to start if timer is done
+    setIsRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning (false);
+    setSecindsLeft (WORK_TIME);
+  };
+
+  // format seconds -> MM:SS
+  const formatTime = (totalSeconds) => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return ' ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}';
+  };
+
   return (
     <div className="timer-card">
 
@@ -14,14 +61,18 @@ function Timer() {
         </button>
       </div>
 
-      <h1 className="timer-display">
-        25:00  { /*need to be change */ }
-      </h1>
+      <h1 className="timer-display"> 
+        {formatTime(secondsLeft)}</h1>
 
       <div className="button-group">
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Reset</button>
+
+        <button onClick = {hanleStart} disabled = {isRUnning}>
+          Start
+          </button>
+        <button onClick = {handlePause} disabled={!isRunning}>
+          Pause
+          </button>
+        <button onClick = {handleReset}>Reset</button>
       </div>
 
     </div>
