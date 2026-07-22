7db8a85aa89eb9542dbc82f6f9b9d91cfd9dbee4
Faisal Benna <fxdedblxe@gmail.com>
2026-07-21 20:53:38 -0700

    Added proper code for the background color to change. Additionally added a css class to have the settings button be closer to the main text

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Timer from "./components/Timer";
 import MusicPlayer from "./components/MusicPlayer";
 import Settings from "./components/Settings";
 
+
 function App() {
+  const [bgColor, setBgColor] = useState("#242424");
+
+
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+  }, [bgColor]);
+
+
   return (
     <div className="app">
-
       <Timer />
-
       <MusicPlayer />
-
-      <Settings />
-
+      <Settings bgColor={bgColor} setBgColor={setBgColor} />
     </div>
   );
 }
+
 
 export default App;
